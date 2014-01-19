4bfa5d945230110ad660a8b38cd6b785f1decb2b
Somber73 <nytesblood@gmail.com>
2014-01-19 18:18:00 -0500

    fb: force carrier label height on first boot
    reported tester issue:
    - boot up
    - pull down QS before any network action was done
    - carrier/wifi label is shown on qs
    
    Expected behaviour: should not show up on QS in any cases
    
    Fix: force hide on first statusbar construct
    
    seems to be in rare cases this update is needed to avoid showing the carrier
    label on QS on first boot up and no connection is there hence emergency label
    is shown

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -1100,6 +1100,7 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
         updateBatteryIcons();
 
         mNetworkController.setListener(this);
+        updateCarrierAndWifiLabelVisibility(true);        
 
         return mStatusBarView;
     }
