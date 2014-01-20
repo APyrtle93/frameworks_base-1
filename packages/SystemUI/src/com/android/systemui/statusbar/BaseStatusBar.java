47c1a54e4447f0c7e4717edeb123eb713816c2b7
somber73 <nytesblood@gmail.com>
2014-01-20 11:49:13 -0500

    Revert "missed import"
    This reverts commit 5c5327451192b4b50f99edcdf2ab65434378180f.

--- packages/SystemUI/src/com/android/systemui/statusbar/BaseStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/BaseStatusBar.java
@@ -43,7 +43,6 @@ import android.graphics.Canvas;
 import android.graphics.Paint;
 import android.graphics.Rect;
 import android.graphics.PixelFormat;
-import android.graphics.PorterDuff;
 import android.graphics.PorterDuffXfermode;
 import android.graphics.PorterDuff.Mode;
 import android.graphics.drawable.BitmapDrawable;
