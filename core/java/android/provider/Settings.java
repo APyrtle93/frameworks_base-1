39095447bc187c409c3c7607bd8fabd877579947
somber73 <nytesblood@gmail.com>
2014-02-06 17:35:13 -0500

    Revert "revert not needed"
    This reverts commit 24b27eaee3cbfffd605477a0ff0242c687160560.

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3798,13 +3798,6 @@ public final class Settings {
          */
         public static final String NOTIFICATION_CUSTOM_CARRIER_LABEL = "notification_custom_carrier_label";
 
-       /**
-        * Whether to control brightness from status bar
-        *
-        * @hide
-        */
-       public static final String STATUS_BAR_BRIGHTNESS_CONTROL = "status_bar_brightness_control";
-		
         /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
