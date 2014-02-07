91d7b4d04f90ba1f43d5143245fbecf8d6a1778f
cphelps76 <cphelps76@droidconcepts.com>
2014-02-07 16:15:10 +0100

    Frameworks: Port BatteryBar (1/2)
    {x} Adds Battery bar with following features:
    {x} location set
    {x} color
    {x} aligment
    {x} thickness
    {x} charging animation
    
    Change-Id: Ie9a168121dc569ec3af068a4bda6b46a4e896b2d
    
    Frameworks: update battery bar default color to match kitkat ui (1/2)
    
    Change-Id: I4fc0b51abb29ac0a9f0b86e4d0ad3519f697b0f6
    
    Conflicts:
    	core/java/android/provider/Settings.java
    	packages/SystemUI/res/layout-sw600dp/navigation_bar.xml
    	packages/SystemUI/res/layout/navigation_bar.xml
    
    Conflicts:
    	core/java/android/provider/Settings.java

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3178,7 +3178,38 @@ public final class Settings {
         public static final String SAFE_HEADSET_VOLUME = "safe_headset_volume";
 
         /**
+<<<<<<< HEAD
          * Whether to mute annoying notifications
+=======
+         * Whether to show the battery bar
+         * @hide
+         */
+        public static final String STATUSBAR_BATTERY_BAR = "statusbar_battery_bar";
+
+        /**
+         * @hide
+         */
+        public static final String STATUSBAR_BATTERY_BAR_COLOR = "statusbar_battery_bar_color";
+
+        /**
+         * @hide
+         */
+        public static final String STATUSBAR_BATTERY_BAR_THICKNESS = "statusbar_battery_bar_thickness";
+
+        /**
+         * @hide
+         */
+        public static final String STATUSBAR_BATTERY_BAR_STYLE = "statusbar_battery_bar_style";
+
+        /**
+         * @hide
+         */
+        public static final String STATUSBAR_BATTERY_BAR_ANIMATE = "statusbar_battery_bar_animate";
+
+        /**
+         * Enable looking up of phone numbers of nearby places
+         *
+>>>>>>> a3e072f... Frameworks: Port BatteryBar (1/2)
          * @hide
          */
         public static final String MUTE_ANNOYING_NOTIFICATIONS_THRESHOLD =
