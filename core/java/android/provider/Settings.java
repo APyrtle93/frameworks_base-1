6a72df9ba79dc37d9b1431c707f9aeffc273d4d7
somber73 <nytesblood@gmail.com>
2014-02-19 20:48:04 -0500

    Revert "New Recents [1/2]"
    This reverts commit e49d11612dd376732f368b0b3ae178ef48601495.

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4035,16 +4035,6 @@ public final class Settings {
         public static final String NOTIFICATION_CUSTOM_CARRIER_LABEL = "notification_custom_carrier_label";
 
         /**
-         * @hide
-         */
-        public static final String LARGE_RECENT_THUMBS = "large_recent_thumbs";
-
-        /**
-         * @hide
-         */
-        public static final String RECENTS_PANEL_COLOR = "recents_panel_color";
-
-        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
