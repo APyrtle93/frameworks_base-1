dd20c34bbf54ad3f208b50ea769d97239a1083b1
Lars Greiss <kufikugel@googlemail.com>
2014-03-08 11:36:29 +0100

    Frameworks: fix the random fc on pinch
    Well logic error from me which can lead in an recycle bitmap fc due of a race
    condition. Reason is simple it the animation needs for whatever reason longer then
    the removing the tasks the view is probably still visible and recycle of the bitmap
    results in a fc.
    
    To fix it just remove the tasks after the animation is done and we definitly know
    that no items are visble which are recycled.
    
    Change-Id: I7878e02e6fb1887f8d11304550e083d107095a4a
    
    Conflicts:
    	packages/SystemUI/src/com/android/systemui/slimrecent/RecentPanelView.java

--- packages/SystemUI/src/com/android/systemui/slimrecent/RecentPanelView.java
+++ packages/SystemUI/src/com/android/systemui/slimrecent/RecentPanelView.java
@@ -326,9 +326,9 @@ public class RecentPanelView {
     }
 
     /**
-* Remove all applications. Call from controller class
-*/
-    protected void removeAllApplications() {
+     * Remove all applications. Call from controller class
+     */
+    protected boolean removeAllApplications() {
         final ActivityManager am = (ActivityManager)
                 mContext.getSystemService(Context.ACTIVITY_SERVICE);
         for (TaskDescription td : mTasks) {
@@ -342,6 +342,7 @@ public class RecentPanelView {
         mTasks.clear();
         mCards.clear();
         mTasksSize = 0;
+        return true;
     }
 
     /**
