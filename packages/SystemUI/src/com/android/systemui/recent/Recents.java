5c13cc5aacee37c6978e4f68d6ca9534813f8b2d
Validus13 <groundzeroroms@gmail.com>
2014-03-24 21:51:10 -0400

    Update Recents.java

--- packages/SystemUI/src/com/android/systemui/recent/Recents.java
+++ packages/SystemUI/src/com/android/systemui/recent/Recents.java
@@ -1,18 +1,18 @@
 /*
- * Copyright (C) 2013 The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+* Copyright (C) 2013 The Android Open Source Project
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
 
 package com.android.systemui.recent;
 
@@ -23,10 +23,7 @@ import android.content.Intent;
 import android.content.res.Configuration;
 import android.content.res.Resources;
 import android.graphics.Bitmap;
-import android.graphics.Canvas;
 import android.graphics.Paint;
-import android.graphics.drawable.BitmapDrawable;
-import android.graphics.drawable.Drawable;
 import android.os.RemoteException;
 import android.os.ServiceManager;
 import android.os.UserHandle;
@@ -37,7 +34,7 @@ import android.view.Display;
 import android.view.IWindowManager;
 import android.view.View;
 
-import com.android.internal.util.slim.DeviceUtils;
+import com.android.internal.util.ose.DeviceUtils;
 
 import com.android.systemui.R;
 import com.android.systemui.RecentsComponent;
@@ -101,14 +98,7 @@ public class Recents extends SystemUI implements RecentsComponent {
                 } catch (RemoteException e) {
                 }
 
-                Bitmap first = null;
-                if (firstTask.getThumbnail() instanceof BitmapDrawable) {
-                    first = ((BitmapDrawable) firstTask.getThumbnail()).getBitmap();
-                } else {
-                    first = Bitmap.createBitmap(1, 1, Bitmap.Config.ARGB_8888);
-                    Drawable d = RecentTasksLoader.getInstance(mContext).getDefaultThumbnail();
-                    d.draw(new Canvas(first));
-                }
+                Bitmap first = firstTask.getThumbnail();
                 final Resources res = mContext.getResources();
 
                 float thumbWidth = res
