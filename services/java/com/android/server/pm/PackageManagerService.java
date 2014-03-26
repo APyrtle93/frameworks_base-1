221d997498797740835e04c88774bfcbc61c5649
somber73 <nytesblood@gmail.com>
2014-03-26 18:46:12 -0400

    Revert "PackageManager: Let com.tmobile.thememanager access privileged permissions"
    This reverts commit 46d6b4b32a8669e2bb6e4150f2afff71e5c8e636.

--- services/java/com/android/server/pm/PackageManagerService.java
+++ services/java/com/android/server/pm/PackageManagerService.java
@@ -1132,8 +1132,7 @@ public class PackageManagerService extends IPackageManager.Stub {
                 ApplicationInfo.FLAG_SYSTEM|ApplicationInfo.FLAG_PRIVILEGED);
         mSettings.addSharedUserLPw("android.uid.shell", SHELL_UID,
                 ApplicationInfo.FLAG_SYSTEM|ApplicationInfo.FLAG_PRIVILEGED);
-        mSettings.addSharedUserLPw("com.tmobile.thememanager", THEME_MAMANER_GUID,
-                ApplicationInfo.FLAG_SYSTEM|ApplicationInfo.FLAG_PRIVILEGED);
+        mSettings.addSharedUserLPw("com.tmobile.thememanager", THEME_MAMANER_GUID, ApplicationInfo.FLAG_SYSTEM);
 
         mPm = (PowerManager) context.getSystemService(Context.POWER_SERVICE);
         
