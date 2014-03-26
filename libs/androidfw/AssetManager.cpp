99c7492ff2eb932065a4f8b11d5649c66feb992d
Validus13 <groundzeroroms@gmail.com>
2014-03-25 20:37:10 -0400

    moar edits

--- libs/androidfw/AssetManager.cpp
+++ libs/androidfw/AssetManager.cpp
@@ -1,19 +1,19 @@
 /*
- * Copyright (C) 2006 The Android Open Source Project
- * This code has been modified.  Portions copyright (C) 2010, T-Mobile USA, Inc.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+* Copyright (C) 2006 The Android Open Source Project
+* This code has been modified. Portions copyright (C) 2010, T-Mobile USA, Inc.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
 
 //
 // Provide access to read-only assets.
@@ -1213,7 +1213,6 @@ AssetDir* AssetManager::openDir(const char* dirName)
 }
 
 /*
-<<<<<<< HEAD
 * Open a directory in the non-asset namespace.
 *
 * An "asset directory" is simply the combination of all files in all
@@ -1222,18 +1221,7 @@ AssetDir* AssetManager::openDir(const char* dirName)
 *
 * Pass in "" for the root dir.
 */
-AssetDir* AssetManager::openNonAssetDir(const int32_t cookie, const char* dirName)
-=======
- * Open a directory in the non-asset namespace.
- *
- * An "asset directory" is simply the combination of all files in all
- * locations, with ".gz" stripped for loose files.  With app, locale, and
- * vendor defined, we have 8 directories and 2 Zip archives to scan.
- *
- * Pass in "" for the root dir.
- */
 AssetDir* AssetManager::openNonAssetDir(void* cookie, const char* dirName)
->>>>>>> 4234fa4... Revert "AssetManager cookies should be int32_t and not void*."
 {
     AutoMutex _l(mLock);
 
