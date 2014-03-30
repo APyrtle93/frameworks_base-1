71856a5dd04e5bd25e3512c8f14c6420f1c55654
somber73 <nytesblood@gmail.com>
2014-03-30 19:21:36 -0400

    custom status bar

--- packages/SystemUI/src/com/android/systemui/statusbar/BaseStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/BaseStatusBar.java
@@ -61,6 +61,7 @@ import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.Paint;
 import android.graphics.Rect;
+import android.graphics.PorterDuff;
 import android.graphics.PixelFormat;
 import android.graphics.PorterDuffXfermode;
 import android.graphics.PorterDuff.Mode;
@@ -232,6 +233,9 @@ public abstract class BaseStatusBar extends SystemUI implements
     // Pie Control
     protected PieController mPieController;
     public NavigationBarOverlay mNavigationBarOverlay;
+	
+    // left-hand icons
+    public LinearLayout mStatusIcons;
 
     // UI-specific methods
 
@@ -289,6 +293,26 @@ public abstract class BaseStatusBar extends SystemUI implements
                 mDeviceProvisioned = provisioned;
                 updateNotificationIcons();
             }
+        }
+    };
+	
+    private class SettingsObserver extends ContentObserver {
+        public SettingsObserver(Handler handler) {
+            super(handler);
+        }
+
+        public void observe() {
+            ContentResolver resolver = mContext.getContentResolver();
+            update();
+        }
+
+        @Override
+        public void onChange(boolean selfChange) {
+            update();
+        }
+
+        private void update() {
+            ContentResolver resolver = mContext.getContentResolver();
         }
     };
 
@@ -439,6 +463,19 @@ public abstract class BaseStatusBar extends SystemUI implements
         mContext.registerReceiver(mBroadcastReceiver, filter);
         SidebarObserver observer = new SidebarObserver(mHandler);
         observer.observe();
+		
+        // Listen for status bar icon color changes
+        mContext.getContentResolver().registerContentObserver(
+            Settings.System.getUriFor(Settings.System.SYSTEM_ICON_COLOR), false, new ContentObserver(new Handler()) {
+                @Override
+                public void onChange(boolean selfChange) {
+                    updateIconColor();
+            }});
+
+        updateIconColor();
+
+        SettingsObserver settingsObserver = new SettingsObserver(new Handler());
+        settingsObserver.observe();
 
         // Listen for HALO enabled switch
         mContext.getContentResolver().registerContentObserver(
@@ -466,6 +503,26 @@ public abstract class BaseStatusBar extends SystemUI implements
         updateHalo();
     }
 
+    private void updateIconColor() {
+        ContentResolver resolver = mContext.getContentResolver();
+
+        boolean mCustomColor = Settings.System.getIntForUser(resolver,
+                Settings.System.CUSTOM_SYSTEM_ICON_COLOR, 0, UserHandle.USER_CURRENT) == 1;
+        int systemColor = Settings.System.getIntForUser(resolver,
+                Settings.System.SYSTEM_ICON_COLOR, -2, UserHandle.USER_CURRENT);
+
+        if (mStatusIcons != null) {
+            for(int i = 0; i < mStatusIcons.getChildCount(); i++) {
+                Drawable iconDrawable = ((ImageView)mStatusIcons.getChildAt(i)).getDrawable();
+                if (mCustomColor) {
+                    iconDrawable.setColorFilter(systemColor, PorterDuff.Mode.SRC_ATOP);
+                } else {
+                    iconDrawable.clearColorFilter();
+                }
+            }
+        }
+    }
+	
     public void setHaloTaskerActive(boolean haloTaskerActive, boolean updateNotificationIcons) {
         mHaloTaskerActive = haloTaskerActive;
         if (updateNotificationIcons) {
